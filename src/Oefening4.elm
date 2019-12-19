e1348c3fae3818eded035d4c1b5f0e8f5b82f1fb
Mike Geenacker <mikegeenacker@gmail.com>
2019-12-19 13:29:05 +0100

    Oefening 4 geupdate

--- src/Oefening4.elm
+++ src/Oefening4.elm
@@ -1,9 +1,9 @@
 module Oefening4 exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, input, li, text, ul)
+import Html exposing (Html, div, input)
 import Html.Attributes exposing (value)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (onInput)
 
 
 type Msg
@@ -26,10 +26,19 @@ main =
         }
 
 
+initModel : Model
 initModel =
     { naam = emptyMM, achternaam = emptyMM }
 
 
+
+-- TODO maak een case expression op model.naam en model.achternaam en genereer
+-- andere view code op basis van de staat van dit ding
+-- zo zou bij de 'Invalid' staat een error message gesdisplayed moeten worden
+-- bij Valid uiteraard niet
+
+
+view : Model -> Html Msg
 view model =
     div []
         [ input [ onInput NaamChanged, value (toStr model.naam) ] []
@@ -37,12 +46,21 @@ view model =
         ]
 
 
+
+-- TODO gebruik de valideer functie om de waardes uit het model te valideren en vervolgens een error in te
+-- vullen als de functie 'false' returnt. Let er op dat je dus nog een aparte validatie functie moet schrijven
+-- dit kan een top level functie of een anonieme zijn (\x -> x / 2 = True)
+
+
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        NaamChanged str ->
+        NaamChanged _ ->
+            -- TODO maak het zodat de als de naam begint met een B hij errort
             model
 
-        AchternaamChanged str ->
+        AchternaamChanged _ ->
+            -- TODO maak het zodat als de achternaam leeg is hij errort
             model
 
 
@@ -51,7 +69,13 @@ update msg model =
 
 
 type alias MinMaxData =
-    { input : String, error : String }
+    { input : String
+    , error : String
+    }
+
+
+
+-- MinMaxVeld kan een Valid of Invalid staat hebben
 
 
 type MinMaxVeld
@@ -59,24 +83,37 @@ type MinMaxVeld
     | Invalid MinMaxData
 
 
+
+-- constructor functie voor een leeg minmaxveld
+
+
+emptyMM : MinMaxVeld
 emptyMM =
     Valid ""
 
 
+
+-- Hulp functie om ons MinxMaxVeld om te zetten naar een string
+
+
+toStr : MinMaxVeld -> String
 toStr minmax =
     case minmax of
         Valid str ->
             str
 
-        Invalid { input, error } ->
+        Invalid { input } ->
             input
+
+
+
+-- De validate functie heeft een functie nodig om te bepalen of hij een valid of invalid veld teruggeeft
 
 
 validate : (String -> Bool) -> String -> MinMaxVeld
 validate valideerFunc input =
-    case valideerFunc input of
-        True ->
-            Valid input
+    if valideerFunc input then
+        Valid input
 
-        False ->
-            Invalid { input = input, error = "Invalid waarde gegeven" }
+    else
+        Invalid { input = input, error = "Invalid waarde gegeven" }
